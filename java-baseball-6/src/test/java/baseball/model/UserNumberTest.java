5e5ff6bc5deee4b703a80d16362e8472eb493c2b
Imdo714 <gusehdla@naver.com>
2025-05-23 11:22:24 +0900

    racingcar-initila-commit

--- java-baseball-6/src/test/java/baseball/model/UserNumberTest.java
+++ java-baseball-6/src/test/java/baseball/model/UserNumberTest.java
@@ -8,15 +8,15 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserNumberTest {
 
-//    @DisplayName("")
-//    @Test
-//    void isEmptyUserNumberValid() {
-//        // given
-//        UserNumber userNumber = new UserNumber();
-//        // when // then
-//        assertThatThrownBy(() -> userNumber.isEmptyUserNumberValid("   "))
-//                .isInstanceOf(IllegalArgumentException.class)
-//                .hasMessage("입력값이 비어 있거나 공백입니다.");
-//    }
+    @DisplayName("")
+    @Test
+    void isEmptyUserNumberValid() {
+        // given
+        UserNumber userNumber = new UserNumber();
+        // when // then
+        assertThatThrownBy(() -> userNumber.isEmptyUserNumberValid("   "))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("입력값이 비어 있거나 공백입니다.");
+    }
 
 }
